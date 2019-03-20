00cf06aeff32f390962be05925a42374860f4ab9
Juan Serra <jiserra@gmail.com>
2019-03-20 14:30:10 -0400

    Check on CVV

--- src/SwipeSimple/InputCVV/InputCVV.js
+++ src/SwipeSimple/InputCVV/InputCVV.js
@@ -6,16 +6,34 @@ import "../swipesimpleInput.css";
 class InputCVVM extends React.Component {
   constructor(props) {
     super(props);
+
+    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
     Payment.formatCardCVC(this.inputRef);
   }
 
+  validCvv(number) {
+    return Payment.fns.validateCardCVC(number);
+  }
+
+  onChange(e) {
+    const value = e.target.value;
+    const isValid = this.validCvv(value);
+
+    if (isValid) {
+      this.inputRef.classList.add("check");
+    } else {
+      this.inputRef.classList.remove("check");
+    }
+  }
+
   render() {
     return (
       <input
         placeholder=""
+        onChange={this.onChange}
         ref={input => (this.inputRef = input)}
         {...this.props}
       />
@@ -24,11 +42,10 @@ class InputCVVM extends React.Component {
 }
 
 function InputCVV(props) {
-  return <InputCVVM {...props} onChange={props.onChange} />;
+  return <InputCVVM {...props} />;
 }
 
 InputCVV.propTypes = {
-  onChange: PropTypes.func,
   disabled: PropTypes.bool
 };
 
